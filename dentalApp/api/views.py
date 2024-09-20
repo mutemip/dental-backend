2c21a9cd6e3298b92ac7a27fa54717cf810dcf32
mutemip <paulmutemi2030@gmail.com>
2024-09-20 12:06:02 +0300

    Housekeeping

--- dentalApp/api/views.py
+++ dentalApp/api/views.py
@@ -19,15 +19,8 @@ from .serializers import (
     )
 
 # Login ViewSet
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny
-
-
 class LoginViewSet(viewsets.ViewSet):
-    permission_classes = [AllowAny]  # Allow anyone to access this view
+    permission_classes = [AllowAny]
 
     def create(self, request):
         """
